f99e0b6c687d58d833bce8ba376eb64e00106c1b
chmaraj <john.chmara@mail.mcgill.ca>
2019-03-28 09:37:47 -0400

    Create Page.java

---
+++ src/ampliconmapper/Page.java
@@ -0,0 +1,15 @@
+package appliconmapper;
+
+public interface Page {
+
+	public void run();
+	
+//	public String[] getOptions();
+	
+//	public void setOptions(String[] options);
+	
+//	public String[] getUserDefined();
+	
+//	public void setUserDefined(String[] userDefined);
+}
+
